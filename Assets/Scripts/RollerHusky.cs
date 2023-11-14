66469f4860303d95f8c9269268610fa82ee21241
junminkyung <minkyung2628@naver.com>
2023-11-14 09:13:02 +0900

    lidar

--- Assets/Scripts/RollerHusky.cs
+++ Assets/Scripts/RollerHusky.cs
@@ -23,7 +23,9 @@ public class RollerHusky : Agent
     {
         aBody.angularVelocity = Vector3.zero;
         aBody.velocity = Vector3.zero;
+        // aBody.TeleportRoot(new Vector3(-0.0f, 0.5f, 0.0f), Quaternion.Euler(0f, 180f, 0f));
         // aBody.TeleportRoot(new Vector3(-3.3f, 0.5f, 3.3f), Quaternion.Euler(0f, 180f, 0f));
+
         float noiseMagnitude = 1.0f; // 노이즈 크기 조절
         Vector3 randomNoise = new Vector3(Random.Range(-noiseMagnitude, noiseMagnitude), 0, Random.Range(-noiseMagnitude, noiseMagnitude));
         Vector3 randomPosition = new Vector3(-3.3f, 0.5f, 3.3f) + randomNoise;
--- Assets/Scripts/TestAgent.cs
+++ Assets/Scripts/TestAgent.cs
@@ -12,7 +12,8 @@ public class TestAgent : Agent
     public float RotSpeed = 3;
     public float LinearSpeed = 5;
     public Transform Target;
-    // public VelodyneSensor LiDAR;
+    public VelodyneSensor LiDAR;
+    private float maxWallDistance = 3.0f;
 
     // public GameObject temp;
     // [SerializeField] private Collider front_bumper;
@@ -33,7 +34,7 @@ public class TestAgent : Agent
         // Agent를 노이즈를 활용해서 (-3.3, 0.5, 3.3)주위의 새로운 무작위 위치에 이동
         float noiseMagnitude = 1.0f; // 노이즈 크기 조절
         Vector3 randomNoise = new Vector3(Random.Range(-noiseMagnitude, noiseMagnitude), 0, Random.Range(-noiseMagnitude, noiseMagnitude));
-        Vector3 randomPosition = new Vector3(-6.0f, 0.2f, 6.0f) + randomNoise;
+        Vector3 randomPosition = new Vector3(-5.5f, 0.2f, 5.5f) + randomNoise;
         Quaternion randomRotation = Quaternion.Euler(0f, Random.Range(0, 360), 0f);
         aBody.TeleportRoot(randomPosition, randomRotation);
 
@@ -44,22 +45,22 @@ public class TestAgent : Agent
 
     public override void CollectObservations(VectorSensor sensor)
     {   
-        // // Velodyne LiDAR 센서에서 수집한 관측 정보를 추가
-        // LiDAR.CompleteJob(); // 센서 작업이 완료될 때까지 기다립니다.
-        // for (int i = 0; i < LiDAR.pointsNum; i++)
-        // {
-        //     float distance = LiDAR.distances[i];
-        //     Vector3 point = LiDAR.points[i];
-        //     float intensity = LiDAR.intensities[i];
+        // Velodyne LiDAR 센서에서 수집한 관측 정보를 추가
+        LiDAR.CompleteJob(); // 센서 작업이 완료될 때까지 기다립니다.
+        for (int i = 0; i < LiDAR.pointsNum; i++)
+        {
+            float distance = LiDAR.distances[i];
+            Vector3 point = LiDAR.points[i];
+            float intensity = LiDAR.intensities[i];
 
-        //     // 디버그 메시지로 관측 정보 출력
-        //     Debug.Log($"LiDAR Observation {i}: Distance = {distance}, Point = {point}, Intensity = {intensity}");
+            // // 디버그 메시지로 관측 정보 출력
+            // Debug.Log($"LiDAR Observation {i}: Distance = {distance}, Point = {point}, Intensity = {intensity}");
 
-        //     // 관측 정보를 ML-Agents 관측 정보로 추가
-        //     sensor.AddObservation(distance);
-        //     sensor.AddObservation(point);
-        //     sensor.AddObservation(intensity);
-        // }
+            // 관측 정보를 ML-Agents 관측 정보로 추가
+            sensor.AddObservation(distance);
+            sensor.AddObservation(point);
+            sensor.AddObservation(intensity);
+        }
 
         // Target/Agent의 위치 정보 수집
         sensor.AddObservation(Target.localPosition);
@@ -76,6 +77,24 @@ public class TestAgent : Agent
         aBody.angularVelocity = aBody.transform.up * Input_Angular_Vel * RotSpeed;
     }
 
+    // 에이전트와 가장 가까운 벽까지의 거리를 반환하는 함수
+    private float GetDistanceToWall()
+    {
+        // 레이캐스트에 사용할 레이어 마스크 (Wall 레이어에만 반응하도록 설정)
+        int wallLayerMask = 1 << LayerMask.NameToLayer("Wall");
+
+        // Raycast를 사용하여 벽까지의 거리를 측정
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxWallDistance, wallLayerMask))
+        {
+            return hit.distance;
+        }
+        else
+        {
+            return maxWallDistance; // 벽이 감지되지 않으면 최대 거리를 반환
+        }
+    }
+    
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         base.OnActionReceived(actionBuffers);
@@ -87,31 +106,40 @@ public class TestAgent : Agent
 
         // Agent와 Target사이의 거리를 측정
         float distanceToTarget = Vector3.Distance(aBody.transform.localPosition, Target.localPosition);
+        // float forwardReward = Mathf.Clamp(aBody.velocity.z, 0.0f, float.MaxValue) * 0.1f;
+
+        // 에이전트와 가장 가까운 벽까지의 거리를 계산
+        float distanceToWall = GetDistanceToWall();
+        float wallPenalty = Mathf.Clamp01(1.0f - distanceToWall / maxWallDistance); // 거리가 작아질수록 값이 커짐
+        float totalReward = 1.2f - wallPenalty; // 보상은 1에서 벽과의 거리에 따라 감소하는 값
 
         // Target에 도달하는 경우 (거리가 1.42보다 작은 경우) Episode 종료
         if (distanceToTarget < 1.42f)
-        {
-            SetReward(1.0f);
-            EndEpisode();
-        }
-
-        // 플랫폼 밖으로 나가면 Episode 종료
-        if (this.transform.localPosition.y < 0)
-        {
-            EndEpisode();
-        }
-
-    }
-
-    void OnCollisionEnter(Collision coll)
-    {
-        if (coll.gameObject.CompareTag("Wall"))
         {   
-            Debug.Log("Hit Wall");
-            // AddReward(-0.1f);
+            // SetReward(1.0f);
+            // SetReward(1.0f + forwardReward);
+            totalReward += 1.0f;
             EndEpisode();
         }
+
+        if (distanceToWall <= 1.2f)
+        {   
+            Debug.Log("Too Close Wall!!");
+            EndEpisode();
+        }
+
+        SetReward(totalReward);
     }
+
+    // void OnCollisionEnter(Collision coll)
+    // {
+    //     if (coll.gameObject.CompareTag("Wall"))
+    //     {   
+    //         Debug.Log("Hit Wall");
+    //         // SetReward(-0.1f + forwardReward);
+    //         EndEpisode();
+    //     }
+    // }
     
     
 
