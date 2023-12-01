643ccdadee2daf1373b54c5f153f9024cc7488c5
junminkyung <minkyung2628@naver.com>
2023-12-01 13:08:43 +0900

    lidar visual add

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///     This script simulates a laser scanner to
+///     detect the surrounding obstacles
+/// </summary>
+public class Laser : MonoBehaviour
+{
+    // general
+    public GameObject laserLink;
+    private Vector3 rayStartPosition;
+    private Vector3 rayStartForward;
+    public bool debugVisualization = false;
+    // scan params
+    public int updateRate = 10;
+    private float scanTime;
+    public int samples = 180;
+    public float angleMin = -Mathf.PI;
+    public float angleMax = Mathf.PI;
+    private float angleIncrement;
+    public float rangeMin = 0.1f;
+    public float rangeMax = 10.0f;
+    // containers
+    private RaycastHit[] raycastHits;
+    private Quaternion[] rayRotations;
+    public float[] directions;
+    public float[] ranges;
+
+    private void Start()
+    {
+        // Containers
+        rayRotations = new Quaternion[samples];
+        directions = new float[samples];
+        ranges = new float[samples];
+
+        // Calculate resolution based on angle limit and number of samples
+        angleIncrement = (angleMax - angleMin) / (samples - 1);
+        for (int i = 0; i < samples; ++i)
+        {
+            directions[i] = angleMin + i * angleIncrement;
+            rayRotations[i] = Quaternion.Euler(
+                new Vector3(0f, directions[i] * Mathf.Rad2Deg, 0f)
+            );
+        }
+
+        // Start scanning
+        scanTime = 1f / updateRate;
+        InvokeRepeating("Scan", 1f, scanTime);
+    }
+
+    private void Scan()
+    {
+        ranges = new float[samples];
+
+        // Cast rays towards diffent directions to find colliders
+        rayStartPosition = laserLink.transform.position;
+        rayStartForward = laserLink.transform.forward;
+        for (int i = 0; i < samples; ++i)
+        {
+            // Ray angle
+            Vector3 rotation = rayRotations[i] * rayStartForward;
+
+            // Check if hit colliders within distance
+            raycastHits = new RaycastHit[samples];
+            if (Physics.Raycast(rayStartPosition, rotation, out raycastHits[i], rangeMax) 
+                && (raycastHits[i].distance >= rangeMin)
+                && (!raycastHits[i].collider.isTrigger))
+            {
+                ranges[i] = raycastHits[i].distance;
+
+                // Visualization
+                if (debugVisualization)
+                {   
+                    Debug.Log($"Ray {i} - Origin: {rayStartPosition}, Direction: {ranges[i] * rotation}");
+                    Debug.DrawRay(
+                        rayStartPosition, ranges[i] * rotation, Color.red, scanTime
+                    );
+                }
+            }
+        }
+    }
+
+    public float[] GetCurrentScanRanges() 
+    {
+        return ranges;
+    }
+}
---
+++ Assets/Scripts/Task2_Roller_laser.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using UnitySensors;
+
+public class Task2_Roller_laser : Agent
+{
+    public ArticulationBody aBody;
+    public float RotSpeed = 3;
+    public float LinearSpeed = 5;
+    public Transform Target;
+    public Laser LiDAR_2D;
+    
+    void Start()
+    {
+    	aBody = GetComponent<ArticulationBody>(); 
+
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        aBody.angularVelocity = Vector3.zero;
+        aBody.velocity = Vector3.zero;
+        aBody.TeleportRoot(new Vector3(-5.5f, 0.0f, 6.0f), Quaternion.Euler(0f, 90f, 0f));
+
+        // Target을 Random함수를 활용해서 새로운 무작위 위치에 이동
+        Target.localPosition = new Vector3(6.0f, 1.8f, -6.5f);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {   
+        // Target/Agent의 위치 정보 수집
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(aBody.transform.localPosition);
+
+        // Agent의 velocity 정보 수집
+        sensor.AddObservation(aBody.velocity.x);
+        sensor.AddObservation(aBody.velocity.z);
+
+        float[] laserScanRanges = LiDAR_2D.GetCurrentScanRanges();
+        foreach (float range in laserScanRanges)
+        {
+            sensor.AddObservation(range);
+        }
+    }
+
+    private void Drive(float Input_Linear_Vel, float Input_Angular_Vel)
+    {
+        aBody.velocity = aBody.transform.forward * Input_Linear_Vel * LinearSpeed;
+        aBody.angularVelocity = aBody.transform.up * Input_Angular_Vel * RotSpeed;
+    }
+    
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        base.OnActionReceived(actionBuffers);
+        var actions = actionBuffers.ContinuousActions;
+        float LinearVel = Mathf.Clamp(actions[0], -1.0f, 1.0f);
+        float AngularVel = Mathf.Clamp(actions[1], -1.0f, 1.0f);
+
+        Drive(LinearVel, AngularVel);
+
+        // Agent와 Target사이의 거리를 측정
+        float distanceToTarget = Vector3.Distance(aBody.transform.localPosition, Target.localPosition);
+   
+        // Target에 도달하는 경우 (거리가 1.42보다 작은 경우) Episode 종료
+        if (distanceToTarget < 1.42f)
+        {   
+            SetReward(1.0f);
+            EndEpisode();
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("Wall"))
+        {   
+            Debug.Log("Hit Wall");
+            EndEpisode();
+        }
+
+        if (coll.gameObject.CompareTag("Partition"))
+        {   
+            Debug.Log("Hit Partition");
+            EndEpisode();
+        }
+    }
+    
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Vertical");
+        continuousActionsOut[1] = Input.GetAxis("Horizontal");
+    }
+}
